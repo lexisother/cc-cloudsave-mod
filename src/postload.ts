6f3fa529e516a35abffc8e0cc62af66afe1e5616
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-27 14:00:21 +0100

    [postload] POST the save to a URL upon saving

--- src/postload.ts
+++ src/postload.ts
@@ -3,7 +3,28 @@ ig.module('cloudsave')
   .defines(() => {
     ig.Storage.inject({
       _saveToStorage() {
-        console.log(ig.storage.slots[0].data);
+        let globals = {} as ig.Storage.GlobalsData;
+        for (let listener of this.listeners) {
+          if (listener.onStorageGlobalSave != null) {
+            listener.onStorageGlobalSave(globals);
+          }
+        }
+
+        const save = {
+          slots: this.slots.map((slot) => slot.data),
+          autoSlot: this.autoSlot != null ? this.autoSlot.data : null,
+          globals,
+          lastSlot: this.lastUsedSlot,
+        };
+        // TODO: Add the API URL after finishing it
+        fetch('', {
+          method: 'post',
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(save),
+        });
         return this.parent();
       },
     });
